54447b8eb59b815622d1895b1f3c43e3f9acb883
unknown <muneebali500@gmail.com>
2021-04-08 09:57:00 +0500

    newsRouter file is modified

--- src/routes/newsRouter.js
+++ src/routes/newsRouter.js
@@ -6,22 +6,34 @@ const router = express.Router();
 
 // GET THE ARTICLES ON FIRST LOAD
 router.get(`/`, async (req, res) => {
-  const response = await axios.get(
-    `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${process.env.NEWS_API_KEY}`
-  );
-  res.render(`news`, { articles: response.data.articles });
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${process.env.NEWS_API_KEY}`
+    );
+    res.render(`news`, { articles: response.data.articles });
+  } catch (err) {
+    if (err) {
+      res.render(`news`, { error: err, articles: null });
+    }
+  }
 });
 
 // GET THE ARTICLES BY SEARCH VALUE
 router.post(`/`, async (req, res) => {
   const search = req.body.search;
 
-  const response = await axios.get(
-    `https://newsapi.org/v2/everything?q=${search.trim()}&apiKey=${
-      process.env.NEWS_API_KEY
-    }`
-  );
-  res.render(`news`, { articles: response.data.articles });
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=${search.trim()}&apiKey=${
+        process.env.NEWS_API_KEY
+      }`
+    );
+    res.render(`news`, { articles: response.data.articles });
+  } catch (err) {
+    if (err) {
+      res.render(`news`, { error: err, articles: null });
+    }
+  }
 });
 
 export default router;
